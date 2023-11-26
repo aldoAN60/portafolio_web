660b3573881a265d8de5703c6465c3f5fe6fb38b
aldo armenta <AldoGuadalupe.Armenta@zkw.mx>
2023-11-26 15:29:11 -0600

    arreglando responsividad y cambiando las imagenes

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,16 +7,9 @@ import { Component } from '@angular/core';
 
 })
 export class EducationComponent {
-  events: any[];
   educations: any[];
 
   constructor() {
-      this.events = [
-          { status: 'Ordered', date: '15/10/2020 10:30', icon: 'pi pi-shopping-cart', color: '#9C27B0', image: 'game-controller.jpg' },
-          { status: 'Processing', date: '15/10/2020 14:00', icon: 'pi pi-cog', color: '#673AB7' },
-          { status: 'Shipped', date: '15/10/2020 16:15', icon: 'pi pi-shopping-cart', color: '#FF9800' },
-          { status: 'Delivered', date: '16/10/2020 10:00', icon: 'pi pi-check', color: '#607D8B' }
-      ];
       this.educations = [
         {course:'java certificate', organization:'SoloLearn', date:'may 2019', course_link:'https://www.sololearn.com/certificates/CT-UF4ZHOYX'},
         {course:'SQL certificate', organization:'SoloLearn', date:'january 2022', course_link:'https://www.sololearn.com/certificates/CT-AUHAASUZ'},
--- src/app/project-slider/project-slider.component.ts
+++ src/app/project-slider/project-slider.component.ts
@@ -12,21 +12,21 @@ export class ProjectSliderComponent implements OnInit{
   projects = [
     {
       "project_name":"License Inventory",
-      "path_picture":['assets/img/license-inventory-pic-1.webp','assets/img/license-inventory-pic-2.png'],
+      "path_picture":['assets/img/license-inventory-pic-1.webp','assets/img/license-inventory-pic-2.webp'],
       "descripcion":"During my internship at ZKW, I led the 'License Inventory' project to address the lack of a formal record of software licenses across the entire company. My responsibilities included gathering information about licenses in different departments, developing a centralized database, and creating the code for both the backend and frontend of the application. I implemented an HTTP server, deployed the application on a local intranet server, and provided an intuitive and secure user interface with a user authentication system. The application enabled the IT department to efficiently manage licenses through a CRUD system.",
       "used_technologies":['Angular','laravel','SQL server'],
 
     },
     {
       "project_name":"Andon 2.0",
-      "path_picture":['assets/img/license-inventory-pic-1.webp','assets/img/license-inventory-pic-2.png'],
+      "path_picture":['assets/img/andon2.0_dashboard.webp','assets/img/andon2.0_line.webp'],
       "descripcion":"During my internship at ZKW, I had the opportunity to lead the development of the web application 'andon 2.0'. This application was planned and designed to transform the way production stoppages are managed, complementing the previous alarm system known as 'andon'. The main focus of 'andon 2.0' is to improve operational efficiency and communication on the production line, optimizing downtime and response times. When a production line stops, the application detects it and sends an email to the responsible personnel. Simultaneously, an alarm is triggered on the production line, and the reason for the stoppage, line number, and elapsed time since the start of the stoppage are displayed on a notification panel located in the offices of operational staff. This multifaceted approach ensures swift communication, immediate response, and detailed information about production stoppages.",
       "used_technologies":['Angular','laravel','SQL server'],
 
     },
     {
       "project_name":"HotelTrack",
-      "path_picture":['assets/img/license-inventory-pic-1.webp','assets/img/license-inventory-pic-2.png'],
+      "path_picture":[],
       "descripcion":"As a result of my previous experience as a receptionist in various hotels, the inspiration to develop the HotelTack project emerged. HotelTack focuses on enhancing task allocation and payment records, addressing key aspects such as efficient room management, detailed tracking of payments related to stays, additional services, and rental of meeting rooms. This project originated from the need to optimize and centralize daily operations in the hotel industry, providing a comprehensive solution that enhances productivity and the overall experience for both staff and customers.",
       "used_technologies":['Larevel','JavaScript','mySQL'],
 
@@ -52,6 +52,6 @@ constructor() {
 
 }
 ngOnInit(): void {
-
+console.log(this.projects[1].path_picture[0]);
   }
 }
